27b2c392896c6c67790f08d414f00025762463e8
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-02 19:43:47 -0300

    feat [app]: defined initial config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
+const routes = require("./routes/router");
 
 const app = express();
+db();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({});
-});
+app.use("/api", routes);
 
 module.exports = app;
