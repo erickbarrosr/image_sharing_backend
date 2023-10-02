0846ad3f6d75410d416ff63ab8469daaa9fe2999
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-02 19:38:52 -0300

    feat: [router]: created public route

--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+// Public Route
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Seja bem vindo!" });
+});
+
 module.exports = router;
