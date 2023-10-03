51345cb87fe795bb14346e421691ed1fa3e84d4b
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-03 18:39:33 -0300

    feat [router]: add route for register users

--- routes/router.js
+++ routes/router.js
@@ -5,4 +5,8 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "Seja bem vindo!" });
 });
 
+// Users Route
+const usersRouter = require("./users");
+router.use("/", usersRouter);
+
 module.exports = router;
