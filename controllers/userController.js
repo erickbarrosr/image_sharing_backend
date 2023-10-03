7c33ec4e78b6895a8eb86b68ff0a1fd2961ab60e
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-03 19:52:19 -0300

    feat [userController]: finished user's register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
+const User = require("../models/User");
 const UserModel = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userController = {
   create: async (req, res) => {
@@ -22,8 +25,46 @@ const userController = {
           .status(422)
           .json({ message: "A confirmação de senha é obrigatória!" });
       }
+
+      const passwordRegex =
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      if (!passwordRegex.test(password)) {
+        return res.status(422).json({
+          message:
+            "A senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra, um número e um caractere especial.",
+        });
+      }
+
+      if (password !== confirmPassword) {
+        return res.status(422).json({ message: "As senhas não conferem!" });
+      }
+
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        return res.status(422).json({ message: "Usuário já existe!" });
+      }
+
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const user = {
+        name,
+        email,
+        password: passwordHash,
+      };
+
+      const userCreated = await UserModel.create(user);
+
+      res
+        .status(201)
+        .json({ userCreated, message: "Usuário criado com sucesso!" });
     } catch (error) {
       console.log(error);
+      res.status(500).json({
+        message:
+          "Aconteceu um erro interno no servidor, por favor tente mais tarde.",
+      });
     }
   },
 };
